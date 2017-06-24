33ead64c1836bd21ea78bbccf636d632f5d5399d
Hans Kurniadi <hans.d.k@live.com>
2017-06-24 01:42:19 -0700

    moved firebase db to dashboard component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+// import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
 import { WebService } from './web.service';
 import { GoogleSigninService } from './google-signin.service';
 
@@ -9,21 +9,19 @@ import { GoogleSigninService } from './google-signin.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  items: FirebaseListObservable<any[]>;
-  // instead of FirebaseListObservable, use tje following
+  // items: FirebaseListObservable<any[]>;
+  // instead of FirebaseListObservable, use the following
   // items: FirebaseObjectObservable<any>;
   constructor (
-    db: AngularFireDatabase,
+    // db: AngularFireDatabase,
     private webService: WebService,
     private googleSigninService: GoogleSigninService
   ) {
     // .list() method (i.e. listening) takes in the node name in the firbase as its parameter
     // This will also return an Observable, so we need to subscribe to it
-    this.items = db.list('/members');
+    // this.items = db.list('/members');
     // this.items = db.object('/members');
-    console.log("Items: ", this.items);
+    // console.log("Items: ", this.items);
   }
   title = 'Mudika Vancouver';
 }
-
-// TODO: add more mock data, move the data retreival to Dashboard component
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { WebService } from '../web.service';
 
 @Component({
@@ -7,23 +8,21 @@ import { WebService } from '../web.service';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  user = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    location: ''
+
+  members: FirebaseListObservable<any[]>;
+
+  constructor(
+    private webService: WebService,
+    db: AngularFireDatabase
+  ) {
+    this.members = db.list('/members');
+    console.log("Members: ", this.members);
   }
 
-  constructor(private webService: WebService) { }
-
-  ngOnInit() {
-    let data = this.webService.getUserData();
-    this.user = {
-      firstName: data.firstName,
-      lastName: data.lastName,
-      email: data.email,
-      location: data.location
-    };
-  }
+  ngOnInit() {}
 
 }
+
+// TODO: 
+// 1. Add buttons in the index page to navigate the router (i.e. dashboard button, news button, etc) 
+// 2. setup login with Google and retrieve some data from it
