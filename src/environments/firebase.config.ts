d5e1333444d2a095befdc82affe5167421fe3b6b
Hans Kurniadi <hans.d.k@live.com>
2017-06-25 14:04:04 -0700

    remove firebase config from repo

--- src/environments/firebase.config.ts
+++
@@ -1,6 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDibenrxRzmhgZcRWSwkMQsOVgC3tlrUWA",
-  authDomain: "mudika-app.firebaseapp.com",
-  databaseURL: "https://mudika-app.firebaseio.com",
-  projectId: "mudika-app",
-};
