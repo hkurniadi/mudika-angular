a3a9fa42ad2532c54ac4b45e1aac392f3415c431
hkurniadi <hans.d.k@live.com>
2017-06-25 14:06:02 -0700

    Delete firebase.config.ts

--- src/environments/firebase.config.ts
+++
@@ -1,6 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCiB2sAp-5iSscv0ljYgdFHS5v5cwgxEw8",
-  authDomain: "mudika-app.firebaseapp.com",
-  databaseURL: "https://mudika-app.firebaseio.com",
-  storageBucket: "mudika-app.appspot.com"
-};
