45cc1ae794ad0e8db345924fc7ebf727b483d318
Hans Kurniadi <hans.d.k@live.com>
2017-06-26 17:26:55 -0700

    fixed redirection after signing in

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,58 +12,49 @@ export class LoginComponent {
 
   user: Observable<firebase.User>;
   googleSigninProvider = new firebase.auth.GoogleAuthProvider();
+  userIsLoggedin: boolean = false;
 
   constructor(
     public afAuth: AngularFireAuth // is equivalent to firebase namespace
   ) {
     this.user = afAuth.authState; // the property returns Observable<firebase.User> (i.e. firebase.user object)
+
+    // This subscription is to check user current state whether loggedin or not (the checking is ongoing since it is an observable)
+    this.user.subscribe(
+      // 'value' is whatever value emitted by afAut.authState whenever it emits new value
+      (value) => {
+        if (value !== null) { // is currentUser object is not null i.e. user is logged in
+          this.userIsLoggedin = true;
+          console.log("User is signed in", value);
+          // TODO: add redirection to fill up more info about the user (e.g. location, school, etc)
+        } else {
+          this.userIsLoggedin = false;
+          console.log("User is not signed in");
+        }
+      },
+      (err) => {
+        console.log("Errors occured", err);
+      },
+      () => {
+        console.log("Finished");
+      }
+    );
   }
 
   login() {
-    // this.webService.login(this.loginPayload);
-
     // AngularFireAuth.auth (i.e. afAuth.auth) returns an initialized firebase.auth.Auth instance
-    
     // returns void, need to call getRedirectResult() to retreive login result
     this.afAuth.auth.signInWithRedirect(this.googleSigninProvider);
-
-    // this.afAuth.auth.getRedirectResult()
-    //     .then(function(result) {
-    //       if (result) {
-    //         console.log("Successfully logged in");
-    //         console.log("Login info:", result);
-    //       }
-    //     })
-    //     .catch(function(err) {
-    //       console.log("Error while loggin in:", err);
-    //     });
-  }
-
-  getRedirectResult() {
-    this.afAuth.auth.getRedirectResult()
-        .then(function(result) {
-          if (result) {
-            console.log("Successfully logged in");
-            console.log("Login info:", result);
-          }
-        })
-        .catch(function(err) {
-          console.log("Error while loggin in:", err);
-        });
-  }
-
-  isLoggedin() {
-    let currentUserState = this.afAuth.auth.currentUser;
-    return currentUserState;
   }
 
   logout() {
     this.afAuth.auth.signOut() // returns void
+    this.userIsLoggedin = false;
   } 
 }
 
 // TODO
-// 1. fix redirection after signing in to check if user is logged in
+// DONE 1. fix redirection after signing in to check if user is logged in
 // 2. connect registered user in Authentication to the database
 // 3. check if the iframe element in the console unsafe 
 
